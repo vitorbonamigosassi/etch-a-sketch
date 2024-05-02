8b693eef1eecb1b5d831bf3f6c9b965998a635a2
Vitor Bonamigo Sassi <vitorbonamigosassi@gmail.com>
2024-05-02 10:33:10 -0300

    Clear grid on button press

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
 const main = document.querySelector("#main");
+const inputButton = document.querySelector("#inputButton");
 
 
-// const GRID_SIZE = 16;
+const GRID_SIZE = 16;
 
-for (let i = 0; i < 16; i++){
+for (let i = 0; i < GRID_SIZE; i++){
     const div = document.createElement("div");
     div.setAttribute("id", "pixel")
     main.appendChild(div);
+}
+
+inputButton.onclick = () => {
+    main.textContent = '';
 }
