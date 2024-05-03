d26a3fbce70ce737261be7efa514aae7f9916a9d
Vitor Bonamigo Sassi <vitorbonamigosassi@gmail.com>
2024-05-03 17:08:02 -0300

    Finish button functionality, change grid behaviour
    The grid function now properly works, creating a grid from the input,
    instead of placing many blocks after each other.

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,24 @@
 const main = document.querySelector("#main");
 const inputButton = document.querySelector("#inputButton");
-
-
-const GRID_SIZE = 16;
-
-for (let i = 0; i < GRID_SIZE; i++){
-    const div = document.createElement("div");
-    div.setAttribute("id", "pixel")
-    main.appendChild(div);
-}
+const inputText = document.querySelector("#inputText");
 
 inputButton.onclick = () => {
-    main.textContent = '';
+   main.textContent = '';
+   if (inputText.value > 100) {
+    inputText.value = "Your max size is 100!";
+   } else {
+    makeRows(inputText.value);
+   }
 }
+
+function makeRows(size) {
+    main.style.setProperty('--grid-cols', size);
+    for (i = 0; i < (size ** 2); i++) {
+      let cell = document.createElement("div");
+      main.appendChild(cell).setAttribute("id", "pixel");
+    };
+  };
+
+
+ 
+  makeRows(16);
