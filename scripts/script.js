9fb0bd40568ac3d726f8fb9af288f994c1543ea5
Vitor Bonamigo Sassi <vitorbonamigosassi@gmail.com>
2024-05-03 19:01:42 -0300

    Add coloring functionality

--- scripts/script.js
+++ scripts/script.js
@@ -8,22 +8,36 @@ inputText.oninput = () => {
 }
 
 inputButton.onclick = () => {
-   main.textContent = '';
-   if (inputText.value > 100) {
-    inputText.value = "Your max size is 100!";
-   } else {
-    makeRows(inputText.value);
-   }
+    main.textContent = '';
+    if (inputText.value > 100) {
+        inputText.value = "Your max size is 100!";
+    } else {
+        makeRows(inputText.value);
+    }
 }
 
 function makeRows(size) {
     main.style.setProperty('--grid-cols', size);
+    const arr = [];
     for (i = 0; i < (size ** 2); i++) {
-      let cell = document.createElement("div");
-      main.appendChild(cell).setAttribute("id", "pixel");
+        let cell = document.createElement("div");
+        main.appendChild(cell).setAttribute("id", "pixel");
+        arr.push(cell);
     };
-  };
+    arr.forEach(pixel => {
+        pixel.addEventListener("mouseup", (e) => {
+            switch (e.button) {
+                case 0:
+                    pixel.style.backgroundColor = "red";
+                    break;
+                case 2:
+                    pixel.style.backgroundColor = "turquoise";
+                    break;
+            }
+        });
+    });
+};
 
 
- 
-  makeRows(16);
+
+makeRows(16);
